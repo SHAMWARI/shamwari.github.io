2433b5e22e24e8384489ebdd9f3dbaec7f94951e
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2021-11-19 13:08:01 +0200

    Add files via upload

--- Handbrake/js/main.js
+++ Handbrake/js/main.js
@@ -2,4 +2,5 @@ $(".menu-burger").on("click", function (e) {
   e.preventDefault;
   $(this).toggleClass("burger-active");
   $(".content").toggleClass("content-active");
+  $(".header__logo").toggleClass("none");
 });
