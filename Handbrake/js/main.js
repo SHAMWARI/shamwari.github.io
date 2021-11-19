e6e1e065e50fb92a38e861c1ed502aa4e98da25d
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2021-11-19 13:24:00 +0200

    Delete Handbrake directory

--- Handbrake/js/main.js
+++
@@ -1,6 +0,0 @@
-$(".menu-burger").on("click", function (e) {
-  e.preventDefault;
-  $(this).toggleClass("burger-active");
-  $(".content").toggleClass("content-active");
-  $(".header__logo").toggleClass("none");
-});
